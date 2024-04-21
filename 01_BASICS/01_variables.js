8a82a414727ddd362356cb1edfacaaf3b28c83f3
Salman Tahir <121248704+Salmantahir002@users.noreply.github.com>
2024-04-21 15:36:26 +0000

    container config done

---
+++ 01_BASICS/01_variables.js
@@ -0,0 +1,10 @@
+const accountNum = 12345;
+let acccountEmail = "salmantahir@gmail.com";
+accountLocation = "Islamabad";
+let accountState;
+/*
+    Prefer not to use var because of issue in block and functional scope
+
+*/
+
+console.table([accountNum, acccountEmail, accountLocation, accountState ]);
