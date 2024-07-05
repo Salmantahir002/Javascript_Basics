a787755634ebcf99199107cd0f1db101fc37bc59
Salman Tahir <121248704+Salmantahir002@users.noreply.github.com>
2024-07-05 16:49:07 +0000

    Conversion&Operations

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,44 @@
+                    // ******************** Conversions *********************
+
+let name = "Salman"
+//console.log(typeof name)
+valueInNumber = Number(name)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//Notes:
+/*
+    Any number in the string value can easily be changed into number For example:
+    "33" can easily be convert into 33 using Number() operator
+    But If "33" has some string value For example "33abc" Javascript will convert it
+    into a number but it will give us NaN value means It is not a number.
+    But the type of NaN will be number.
+*/
+
+/*
+    true will be 1
+    False will be 0
+    For example:
+*/
+
+let IsLoginIn = 1
+let BooleanLoginIn = Boolean(IsLoginIn)
+//console.log(BooleanLoginIn);
+
+// "" Empty string is False
+// "Salman" With some string value is True.
+
+                    // ********************** Operations *****************************
+
+let value = 5
+let NegativeValue = -value
+// console.log(NegativeValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// console.log((3+4) * 5 / 7);
+
