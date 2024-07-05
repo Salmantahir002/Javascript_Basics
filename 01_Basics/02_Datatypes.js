d460c5e1abdb564923b085f328084ab091941010
Salman Tahir <121248704+Salmantahir002@users.noreply.github.com>
2024-07-05 15:58:30 +0000

    Datatypes

--- 01_Basics/02_Datatypes.js
+++ 01_Basics/02_Datatypes.js
@@ -1,3 +1,15 @@
 // CLass 02: Datatypes
 
 //Datatypes in Javascript:
+
+let name = "Salman" // String Datatype
+let age = 21 // Number Datatype
+let isLogin = false // Boolean Datatype
+let work; // undefined Datatype
+let state = null // Stand alone Value // Its a object type value
+//symbol Datatype Use for uniqueness.
+
+// If we have to find the type of any data type we use;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof null); // object Datatype
