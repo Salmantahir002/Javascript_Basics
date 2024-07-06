e5989220c964ab9a49b91ae52b01e669fe326e8b
Salman Tahir <121248704+Salmantahir002@users.noreply.github.com>
2024-07-06 10:16:01 +0000

    comparisons

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,17 @@
+// ***Comparison operators:***
+
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==2);
+// console.log(2!=2);
+// console.log(2>=1);
+
+// console.log("2" > 1);
+console.log(null < 0);
+console.log(null == 0);
+console.log(null >= 0);
+console.log(null === 0);// strict equality
+/*  Notes:
+    Equality check == and comparison operator >,<,>= works differently.
+    Comparisons convert null to a number treating it as a 0.
+ */
